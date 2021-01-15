c1ebb6ab166fd2b4775959e28d7fcce95233e875
DenisLopatin <www.denis-lopatin@yandex.ru>
2021-01-16 02:09:09 +0300

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,16 @@
 import '../index.html';
 import pageScrollIndicator from './page-scroll-indicator';
 
-window.pageScrollIndicator = pageScrollIndicator;
+pageScrollIndicator({
+    element: '.scroll-page-indicator',
+    zIndex: 10000,
+    height: 10,
+    opacity: 1,
+    backgroundColor: 'aqua',
+});
+
+if (typeof module === 'object' && typeof module.exports === 'object') {
+    module.exports = pageScrollIndicator;
+} else {
+    window.pageScrollIndicator = pageScrollIndicator;
+}
