e96cc1933928668ee1e8f19e4f17596a09fd5194
DenisLopatin <www.denis-lopatin@yandex.ru>
2021-01-15 21:59:47 +0300

    pu

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'development-page-scroll-indicator.js',
+        filename: 'page-scroll-indicator.js',
         publicPath: '',
     },
     target: isDev ? 'web' : 'browserslist',
