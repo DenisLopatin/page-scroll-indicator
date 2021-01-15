07f7a46bd386b6b19e8808527dc4da96066253fe
DenisLopatin <www.denis-lopatin@yandex.ru>
2021-01-15 17:12:21 +0300

    plugin create

--- src/scripts/page-scroll-indicator.js
+++ src/scripts/page-scroll-indicator.js
@@ -1,4 +1,10 @@
-function scroll(scrollPageIndicator) {
+/** function scroll
+ *  calculates the scroll height of the document
+ *  @param { HTMLElement } scrollPageIndicator - HTMLElement div for scroll
+ *  @param { number } margins - number calculated from fields
+ * */
+
+function scroll(scrollPageIndicator, margins) {
     try {
         const documentHeightValues = [
             document.body.scrollHeight,
@@ -11,6 +17,7 @@ function scroll(scrollPageIndicator) {
         const documentHeight = Math.min(...documentHeightValues);
         const documentFullHeight = Math.max(...documentHeightValues);
         const finalHeight = documentFullHeight - documentHeight;
+        if (finalHeight === margins) return;
         const scrollTop = window.pageYOffset;
         const result = Math.round((scrollTop * 100) / finalHeight);
         scrollPageIndicator.style.right = `${String(100 - result)}%`;
@@ -18,6 +25,39 @@ function scroll(scrollPageIndicator) {
         throw new Error(err);
     }
 }
+
+/** function getMargins
+ *  calculates the fields of the body element and all its first-level child elements
+ *  @return { number } - returns the maximum value
+ * */
+
+function getMargins() {
+    const bodyMargins = [
+        Number(getComputedStyle(document.body).marginTop.replace('px', '')),
+        Number(getComputedStyle(document.body).marginBottom.replace('px', '')),
+    ].reduce((acc, el) => {
+        acc += el;
+        return acc;
+    }, 0);
+    const bodyElementsMargins = [
+        ...document.querySelectorAll('body > *'),
+    ].reduce((acc, el) => {
+        const elMarginTop = getComputedStyle(el).marginTop.replace('px', '');
+        const elMarginBottom = getComputedStyle(el).marginBottom.replace(
+            'px',
+            '',
+        );
+        const marginValue = Number(elMarginTop) + Number(elMarginBottom);
+        acc += marginValue;
+        return acc;
+    }, 0);
+    return Math.max(bodyMargins, bodyElementsMargins);
+}
+
+/** function pageScrollIndicator
+ *   config function
+ *   @param { Object } config - configuration
+ * */
 
 function pageScrollIndicator(config) {
     try {
@@ -39,7 +79,9 @@ function pageScrollIndicator(config) {
             config.transition || '300ms linear'
         }`;
 
-        window.addEventListener('scroll', scroll.bind(null, element));
+        const margins = getMargins();
+
+        window.addEventListener('scroll', scroll.bind(null, element, margins));
     } catch (err) {
         throw new Error(err);
     }
