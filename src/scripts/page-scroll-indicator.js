ffad3727446ea326b82d3e1c9e82061e0766ac8d
DenisLopatin <www.denis-lopatin@yandex.ru>
2021-02-26 21:10:47 +0300

    update v.2.0.0

--- src/scripts/page-scroll-indicator.js
+++ src/scripts/page-scroll-indicator.js
@@ -1,8 +1,4 @@
 import '../index.html';
-import jQuery from 'jquery';
 import development from './development';
 
 Element.prototype.pageScrollIndicator = development;
-(function jq($) {
-    $.fn.pageScrollIndicator = development;
-}(jQuery));
